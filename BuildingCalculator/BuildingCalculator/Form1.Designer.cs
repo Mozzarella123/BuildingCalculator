c1b9e4467f2a30c99188f3c028f3ff1a4bef46c0
Рогов Никита <Nikita1998_2010@mail.ru>
2017-06-15 21:39:36 +0300

    Компонент слайдера + параметры комнаты

--- BuildingCalculator/BuildingCalculator/Form1.Designer.cs
+++ BuildingCalculator/BuildingCalculator/Form1.Designer.cs
@@ -28,206 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Left = new System.Windows.Forms.Button();
-            this.Right = new System.Windows.Forms.Button();
-            this.Width = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.slider1 = new BuildingCalculator.Slider();
+            this.slider2 = new BuildingCalculator.Slider();
+            this.WidthT = new System.Windows.Forms.Label();
+            this.LengthT = new System.Windows.Forms.Label();
             this.Length = new System.Windows.Forms.TextBox();
-            this.Add = new System.Windows.Forms.Button();
-            this.Remove = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.CurrentIndex = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.Width = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Left
+            // slider1
             // 
-            this.Left.BackColor = System.Drawing.Color.Gainsboro;
-            this.Left.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Left.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
-            this.Left.FlatAppearance.BorderSize = 0;
-            this.Left.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Left.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Left.Location = new System.Drawing.Point(3, 201);
-            this.Left.Name = "Left";
-            this.Left.Size = new System.Drawing.Size(45, 35);
-            this.Left.TabIndex = 0;
-            this.Left.Text = "<";
-            this.Left.UseVisualStyleBackColor = false;
-            this.Left.Click += new System.EventHandler(this.Slider_Controls);
+            this.slider1.Location = new System.Drawing.Point(429, 48);
+            this.slider1.Name = "slider1";
+            this.slider1.Size = new System.Drawing.Size(144, 247);
+            this.slider1.TabIndex = 20;
             // 
-            // Right
+            // slider2
             // 
-            this.Right.BackColor = System.Drawing.Color.Gainsboro;
-            this.Right.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Right.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
-            this.Right.FlatAppearance.BorderSize = 0;
-            this.Right.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Right.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Right.Location = new System.Drawing.Point(105, 201);
-            this.Right.Name = "Right";
-            this.Right.Size = new System.Drawing.Size(47, 35);
-            this.Right.TabIndex = 2;
-            this.Right.Text = ">";
-            this.Right.UseVisualStyleBackColor = false;
-            this.Right.Click += new System.EventHandler(this.Slider_Controls);
+            this.slider2.Location = new System.Drawing.Point(618, 48);
+            this.slider2.Name = "slider2";
+            this.slider2.Size = new System.Drawing.Size(144, 247);
+            this.slider2.TabIndex = 21;
+            // 
+            // WidthT
+            // 
+            this.WidthT.AutoSize = true;
+            this.WidthT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WidthT.Location = new System.Drawing.Point(17, 167);
+            this.WidthT.Name = "WidthT";
+            this.WidthT.Size = new System.Drawing.Size(105, 29);
+            this.WidthT.TabIndex = 22;
+            this.WidthT.Text = "Ширина";
+            // 
+            // LengthT
+            // 
+            this.LengthT.AutoSize = true;
+            this.LengthT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LengthT.Location = new System.Drawing.Point(26, 75);
+            this.LengthT.Name = "LengthT";
+            this.LengthT.Size = new System.Drawing.Size(87, 29);
+            this.LengthT.TabIndex = 23;
+            this.LengthT.Text = "Длина";
+            // 
+            // Length
+            // 
+            this.Length.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Length.Location = new System.Drawing.Point(17, 116);
+            this.Length.MaxLength = 4;
+            this.Length.Multiline = true;
+            this.Length.Name = "Length";
+            this.Length.Size = new System.Drawing.Size(105, 38);
+            this.Length.TabIndex = 24;
             // 
             // Width
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.Width, 3);
-            this.Width.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Width.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Width.Location = new System.Drawing.Point(5, 154);
-            this.Width.Margin = new System.Windows.Forms.Padding(5);
+            this.Width.Location = new System.Drawing.Point(17, 211);
             this.Width.MaxLength = 4;
             this.Width.Multiline = true;
             this.Width.Name = "Width";
-            this.Width.Size = new System.Drawing.Size(145, 39);
-            this.Width.TabIndex = 6;
-            this.Width.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Width.TextChanged += new System.EventHandler(this.Params_Input);
+            this.Width.Size = new System.Drawing.Size(105, 38);
+            this.Width.TabIndex = 25;
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.tableLayoutPanel1.SetColumnSpan(this.label3, 3);
-            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(3, 119);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 30);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Ширина";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(464, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 29);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Двери";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.Control;
-            this.tableLayoutPanel1.SetColumnSpan(this.label2, 3);
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(655, 8);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 30);
-            this.label2.TabIndex = 15;
-            this.label2.Text = "Длина";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Size = new System.Drawing.Size(72, 29);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Окна";
             // 
-            // Length
+            // label3
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.Length, 3);
-            this.Length.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Length.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Length.Location = new System.Drawing.Point(5, 75);
-            this.Length.Margin = new System.Windows.Forms.Padding(5);
-            this.Length.MaxLength = 4;
-            this.Length.Multiline = true;
-            this.Length.Name = "Length";
-            this.Length.Size = new System.Drawing.Size(145, 39);
-            this.Length.TabIndex = 14;
-            this.Length.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Length.TextChanged += new System.EventHandler(this.Params_Input);
-            // 
-            // Add
-            // 
-            this.Add.BackColor = System.Drawing.Color.Gainsboro;
-            this.Add.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Add.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
-            this.Add.FlatAppearance.BorderSize = 0;
-            this.Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Add.Location = new System.Drawing.Point(105, 3);
-            this.Add.Name = "Add";
-            this.Add.Size = new System.Drawing.Size(47, 34);
-            this.Add.TabIndex = 17;
-            this.Add.Text = "+";
-            this.Add.UseVisualStyleBackColor = false;
-            this.Add.Click += new System.EventHandler(this.Slider_Controls);
-            // 
-            // Remove
-            // 
-            this.Remove.BackColor = System.Drawing.Color.Gainsboro;
-            this.Remove.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Remove.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
-            this.Remove.FlatAppearance.BorderSize = 0;
-            this.Remove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Remove.Location = new System.Drawing.Point(3, 3);
-            this.Remove.Name = "Remove";
-            this.Remove.Size = new System.Drawing.Size(45, 34);
-            this.Remove.TabIndex = 16;
-            this.Remove.Text = "-";
-            this.Remove.UseVisualStyleBackColor = false;
-            this.Remove.Click += new System.EventHandler(this.Slider_Controls);
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.Controls.Add(this.Remove, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.Right, 2, 5);
-            this.tableLayoutPanel1.Controls.Add(this.Left, 0, 5);
-            this.tableLayoutPanel1.Controls.Add(this.CurrentIndex, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.Add, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.Length, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.Width, 0, 4);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(155, 239);
-            this.tableLayoutPanel1.TabIndex = 19;
-            // 
-            // CurrentIndex
-            // 
-            this.CurrentIndex.AutoSize = true;
-            this.CurrentIndex.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CurrentIndex.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.CurrentIndex.Location = new System.Drawing.Point(54, 0);
-            this.CurrentIndex.Name = "CurrentIndex";
-            this.CurrentIndex.Size = new System.Drawing.Size(45, 40);
-            this.CurrentIndex.TabIndex = 18;
-            this.CurrentIndex.Text = "1";
-            this.CurrentIndex.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 29);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Комната";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 462);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(774, 587);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Width);
+            this.Controls.Add(this.Length);
+            this.Controls.Add(this.LengthT);
+            this.Controls.Add(this.WidthT);
+            this.Controls.Add(this.slider2);
+            this.Controls.Add(this.slider1);
             this.Name = "Form1";
             this.Text = "Калькулятор";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Left;
-        private System.Windows.Forms.Button Right;
-        private System.Windows.Forms.TextBox Width;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private Slider slider1;
+        private Slider slider2;
+        private System.Windows.Forms.Label WidthT;
+        private System.Windows.Forms.Label LengthT;
         private System.Windows.Forms.TextBox Length;
-        private System.Windows.Forms.Button Add;
-        private System.Windows.Forms.Button Remove;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label CurrentIndex;
+        private System.Windows.Forms.TextBox Width;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
