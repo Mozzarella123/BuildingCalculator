5269b46be146fbf5cfc1f58fcd50f2bb478b1817
slon <serafim.gerasimov@mail.ru>
2017-06-18 19:36:49 +0300

    Завершена система сборки делегатов

---
+++ BuildingCalculator/BuildingCalculator/ForCompile.cs
@@ -0,0 +1,12 @@
+using System;
+namespace MyNamespace
+{
+    
+    public static class LambdaCreator 
+    {
+        
+        public static double f0(double площадь){ return площадь*50;}
+        public static double f1(double площадь,double периметр){ return площадь*30+периметр*20;}
+        
+    }
+}
