2ae401f0bbe5ca8fcce44cb953a14cddd0b0f972
Рогов Никита <Nikita1998_2010@mail.ru>
2017-06-19 14:05:38 +0300

    Написан валидатор

--- BuildingCalculator/BuildingCalculator/Classes/Function.cs
+++ BuildingCalculator/BuildingCalculator/Classes/Function.cs
@@ -9,6 +9,14 @@ namespace BuildingCalculator.Classes
 {
     public static class Functions
     {
+        public enum ValidateType { OnlyText,OnlyNumbers, UserValidate }
+        /// <summary>
+        /// Создает контекстное меню для любого элемента
+        /// </summary>
+        /// <param name="control">Control к которому нужно привязать меню</param>
+        /// <param name="names">Названия элементов</param>
+        /// <param name="funcs">Функции элементов</param>
+        /// <returns>Возвращает созданное контекстное меню</returns>
         public static ContextMenuStrip ContextMenu(Control control,List<string> names,List<EventHandler> funcs)
         {           
             ContextMenuStrip menu = new ContextMenuStrip();
@@ -19,6 +27,48 @@ namespace BuildingCalculator.Classes
             }
             control.ContextMenuStrip = menu;
             return menu;         
-        }       
+        }
+        /// <summary>
+        /// Создать валидатор для Control
+        /// </summary>
+        /// <param name="type">Тип валидации</param>
+        /// <param name="control">Control, к которому привязать</param>
+        /// <param name="userfunc">Собственный обработчик</param>
+        public static void Validate(ValidateType type, Control control, KeyPressEventHandler userfunc = null)
+        {
+            switch (type)
+            {
+                case ValidateType.OnlyNumbers:
+                    {
+                        control.KeyPress += ValidNumbers;
+                        break;
+                    }
+                case ValidateType.OnlyText:
+                    {
+                        control.KeyPress += ValidText;
+                        break;
+                    }
+                case ValidateType.UserValidate:
+                    {
+                        if (userfunc != null)
+                            control.KeyPress += userfunc;
+                        break;
+                    }                    
+            }
+        }
+        private static void ValidNumbers (object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b')
+                e.Handled = true;
+            else
+                if ((sender as Control).Text == "0")
+                (sender as Control).Text = "";
+        }
+        private static void ValidText(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsLetter(e.KeyChar) && e.KeyChar != '\b')
+                e.Handled = true;
+        }
+
     }
 }
