dcde670496b814a30c9bb3355c18b36ffcaa0e54
Рогов Никита <Nikita1998_2010@mail.ru>
2017-06-27 13:21:36 +0300

    Доработки в дизайне+ имзенение названия комнаты

---
+++ BuildingCalculator/BuildingCalculator/FormComponents/ChangeName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildingCalculator.FormComponents
+{
+    public partial class ChangeName : Form
+    {
+        public ChangeName()
+        {
+            InitializeComponent();
+            textBox1.KeyDown += ChangeName_KeyDown;
+            
+        }
+
+        private void ChangeName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter||e.KeyCode==Keys.Escape)
+                Dispose();
+        }
+    }
+}
